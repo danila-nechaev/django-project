c5167240778008b99bd441050254640a3aaca035
Danila Nechaev <danila-nechaev-2001@mail.ru>
2023-03-22 20:20:43 +0300

    first commit

---
+++ w_project/main/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+# Create your views here.
+
+def index(request):
+    tasks = Task.objects.all()# Из таблицы Таск получать все данные и перелавать в нужный шаблон Если мы хотим сортировать то пишем ордер бай(поле по которому хотим сортировать)
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks':tasks})
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Формат неверный'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error}
+    return render(request, 'main/create.html', context)
+
